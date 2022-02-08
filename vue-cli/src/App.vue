f4787bb907646d2aa7a776e23609e9a95aa9059e
Anton Liubimov <ant.liubimov@gmail.com>
2022-02-08 20:48:57 +0500

    first commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+     <div class="wrapper-content">
+       <section>
+         <div class="container">
+           <h1>HELLO</h1>
+        <img alt="Vue logo" src="./assets/logo.png">
+         </div>
+       </section>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
