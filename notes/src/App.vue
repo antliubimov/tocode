a442e9694147cf95de7ab3210ee3c53037e58010
Anton Liubimov <ant.liubimov@gmail.com>
2022-02-08 22:51:46 +0500

    added components

---
+++ notes/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="wrapper">
+		<div class="wrapper-content">
+			<section>
+				<div class="container">
+					<h1>{{ title }}</h1>
+					<message v-if="message" :message="message" />
+
+					<newNote :note="note" @addNote="addNote" />
+
+					<notes :notes="notes" @remove="removeNote" />
+				</div>
+			</section>
+		</div>
+	</div>
+</template>
+
+<script>
+import message from "@/components/Message.vue";
+import newNote from "@/components/NewNote.vue";
+import notes from "@/components/Notes.vue";
+
+export default {
+	name: "App",
+	components: {
+		message,
+		newNote,
+		notes,
+	},
+	data() {
+		return {
+			title: "Notes App",
+			message: null,
+			note: {
+				title: "",
+				descr: "",
+			},
+			notes: [
+				{
+					title: "First Note",
+					descr: "Description for first note",
+					date: new Date(Date.now()).toLocaleString(),
+				},
+				{
+					title: "Second Note",
+					descr: "Description for second note",
+					date: new Date(Date.now()).toLocaleString(),
+				},
+				{
+					title: "Third Note",
+					descr: "Description for third note",
+					date: new Date(Date.now()).toLocaleString(),
+				},
+			],
+		};
+	},
+	methods: {
+		addNote() {
+			let { title, descr } = this.note;
+			if (title === "") {
+				this.message = "title can`t be blank!";
+				return false;
+			}
+			this.notes.push({
+				title,
+				descr,
+				date: new Date(Date.now()).toLocaleString(),
+			});
+			this.message = null;
+			this.note.title = "";
+			this.note.descr = "";
+		},
+		removeNote(index) {
+			this.notes.splice(index, 1);
+		},
+	},
+};
+</script>
+
+<style></style>
---
+++ notes/src/components/Notes.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="notes">
+		<div class="note" v-for="(note, index) in notes" :key="index">
+			<div class="note-header">
+				<p>{{ note.title }}</p>
+				<p style="cursor: pointer" @click="removeNote(index)">x</p>
+			</div>
+			<div class="note-body">
+				<p>{{ note.descr }}</p>
+				<span>{{ note.date }}</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		notes: {
+			type: Array,
+			required: true,
+		},
+	},
+	methods: {
+		removeNote(index) {
+			this.$emit("remove", index);
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	padding: 40px 0;
+}
+
+.note {
+	width: 48%;
+	padding: 18px 20px;
+	margin-bottom: 20px;
+	background-color: #ffffff;
+}
+.note-header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	p {
+		font-size: 22px;
+		color: #402caf;
+	}
+}
+
+.note-body {
+	p {
+		margin: 20px 0;
+	}
+	span {
+		font-size: 14px;
+		color: #999999;
+	}
+}
+</style>
